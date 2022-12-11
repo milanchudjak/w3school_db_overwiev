3fac8bb9aa0c8c03ffa8b83f6c0e5b07090d4dad
Milan Chudjak <mchudjak@gmail.com>
2022-12-11 20:50:48 +0000

    Counts of Cities and Countries added.

---
+++ basic_statistics/num_cities_overall.sql
@@ -0,0 +1,7 @@
+-- How many cities are the customers in the Customers table from?
+
+
+SELECT COUNT(DISTINCT City) AS NumDistinctCities 
+FROM Customers
+
+-- Result: 69 City
---
+++ basic_statistics/num_countries_overall.sql
@@ -0,0 +1,8 @@
+-- How many countries are the customers in the Customers table from?
+
+SELECT COUNT(DISTINCT Country) AS NumDistinctCountries   
+FROM Customers
+
+-- Result: 21 Countries
+
+
